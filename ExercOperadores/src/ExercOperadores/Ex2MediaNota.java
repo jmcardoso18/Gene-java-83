51662614e1dabaa70a5da1b3910d56a1426bcbda
Jamila Moraes Cardoso <jmc18.ads@gmail.com>
2025-09-12 11:58:35 -0300

    Lista de exercicio Operadores

---
+++ ExercOperadores/src/ExercOperadores/Ex2MediaNota.java
@@ -0,0 +1,30 @@
+package ExercOperadores;
+
+import java.util.Scanner;
+
+public class Ex2MediaNota {
+	public static void main(String[] args) {
+		//Instânciando o objeto Scanner
+		Scanner leia = new Scanner(System.in);
+		
+		//Declarando variáveis
+		float nota1,nota2,nota3,nota4,media;
+		
+		//Solicitando entrada de dados
+		System.out.println("Digite sua primeira nota:");
+		nota1=leia.nextFloat();
+		System.out.println("Digite sua segunda nota:");
+		nota2=leia.nextFloat();
+		System.out.println("Digite sua terceira nota:");
+		nota3=leia.nextFloat();
+		System.out.println("Digite sua quarta nota:");
+		nota4=leia.nextFloat();
+		media=(nota1+nota2+nota3+nota4)/4;
+		
+		//Exibindo a saida de dados
+		System.out.printf("Sua média final: %.1f",media);
+		
+		//Fechando o objeto Scanner
+		leia.close();
+	}
+}
